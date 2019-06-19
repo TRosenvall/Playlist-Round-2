26816ee95cbe95dc9fe6e033548a428a7cea3979
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2019-06-19 11:29:31 -0600

    Finished Models

---
+++ Playlist Round 2/Models/Playlist+Convenience.swift
@@ -0,0 +1,17 @@
+//
+//  Playlist+Convenience.swift
+//  Playlist Round 2
+//
+//  Created by Timothy Rosenvall on 6/19/19.
+//  Copyright © 2019 Timothy Rosenvall. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Playlist {
+    convenience init(name: String, context: NSManagedObjectContext = CoreDataStack.managedObjectContext) {
+        self.init(context:context)
+        self.name = name
+    }
+}
---
+++ Playlist Round 2/Models/Song+Convenience.swift
@@ -0,0 +1,19 @@
+//
+//  Song+Convenience.swift
+//  Playlist Round 2
+//
+//  Created by Timothy Rosenvall on 6/19/19.
+//  Copyright © 2019 Timothy Rosenvall. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Song {
+    convenience init(title: String, artist: String, playlist: Playlist, context: NSManagedObjectContext = CoreDataStack.managedObjectContext) {
+        self.init(context: context)
+        self.title = title
+        self.artist = artist
+        self.playlist = playlist
+    }
+}
